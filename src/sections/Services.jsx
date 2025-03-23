3de966f1d6a74b919f3e732745735913a3e62845
Emmanuel K. Mensah <ekmpizarro@gmail.com>
2025-03-23 12:12:18 +0000

    Changed accent color for the services section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -13,11 +13,11 @@ const Services = () => {
         variants={slideUpVariants}
         className='lg:w-[80%] w-[90%] m-auto py-[60px] flex flex-col justify-between items-center gap-[20px]'
       >
-        <motion.h1 variants={slideUpVariants} className='text-yellow-500 text-2xl'>SPECIAL OFFER</motion.h1>
+        <motion.h1 variants={slideUpVariants} className='text-turquoise text-2xl'>SPECIAL OFFER</motion.h1>
         <motion.h1 variants={slideUpVariants} className='text-black uppercase text-[40px] font-bold text-center'>OUR BEST SERVICES</motion.h1>
         <motion.div
           variants={slideUpVariants}
-          className='w-[120px] h-[6px] bg-yellow-500'
+          className='w-[120px] h-[6px] bg-turquoise'
         ></motion.div>
 
         {/* make div for services mapping from export js file */}
@@ -33,7 +33,7 @@ const Services = () => {
                 variants={zoomInVariants}
                 className='flex justify-center items-start gap-5 p-8'
                 key={index}>
-                <img src={item.icon} alt="icon" className='w-[80px] border-2 border-yellow-500 hover:bg-yellow-500 rounded-lg p-2' />
+                <img src={item.icon} alt="icon" className='w-[80px] border-2 border-turquoise hover:bg-turquoise rounded-lg p-2' />
                 <div className='flex flex-col justify-center items-start gap-3'>
                   <h1 className='text-xl font-bold text-black'>{item.title}</h1>
                   <p className='text-[18px]'>{item.about}</p>
