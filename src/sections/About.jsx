c9fde65ba835892ec7a17e2d76c05afe8d1e86fc
Emmanuel K. Mensah <ekmpizarro@gmail.com>
2025-03-23 10:19:24 +0000

    Modified typography in the about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,52 +1,70 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import { motion } from 'framer-motion'
-import { slideUpVariants, zoomInVariants } from './animation'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { motion } from "framer-motion";
+import { slideUpVariants, zoomInVariants } from "./animation";
+import { useNavigate } from "react-router-dom";
 
 const About = () => {
   const navigate = useNavigate();
 
   return (
-    <div className='lg:w-[80%] w-[90%] m-auto py-[60px] flex lg:flex-row flex-col justify-between items-start gap-[50px]' id='about'>
+    <div
+    id="about"
+    className="flex flex-col justify-between w-[90%] m-auto py-10 gap-10 items-start lg:w-[80%] lg:flex-row"
+    >
       <motion.div
         initial="hidden"
         whileInView="visible"
         variants={slideUpVariants}
-        className='lg:w-[60%] w-full flex flex-col justify-center items-start lg:gap-6'
+        className="w-full flex flex-col  justify-center items-center gap-3 md:items-start lg:w-[60%] lg:gap-6"
       >
         <motion.h1
-          variants={slideUpVariants} className='text-turquoise text-2xl'
-        >WELCOME TO</motion.h1>
+          variants={slideUpVariants}
+          className="text-2xl text-turquoise md:text-left md:text-3xl"
+        >
+          WELCOME TO
+        </motion.h1>
         <motion.h1
           variants={slideUpVariants}
-          className='text-white uppercase text-[35px] font-bold'
-        >DEMOLAHU DRAUGHTSMANSHIP & CONSTRUCTION LTD.
+          className="text-white uppercase text-2xl text-center font-semibold md:text-left md:text-3xl lg:text-4xl"
+        >
+          DEMOLAHU DRAUGHTSMANSHIP & CONSTRUCTION LTD.
         </motion.h1>
-        <div className='w-[120px] h-[6px] bg-turquoise'></div>
-        <p className='text-2xl italic text-gray-50 mt-[60px]'>Your trusted partner in architectural design, construction, and land development. We transform visions into reality with precision, innovation, and excellence.</p>
+        <div className="w-[120px] h-1.5 bg-turquoise"></div>
+        <p className="text-xl italic text-center text-gray-50 md:text-2xl md:text-left">
+          Your trusted partner in architectural design, construction, and land
+          development. We transform visions into reality with precision,
+          innovation, and excellence.
+        </p>
       </motion.div>
 
       <motion.div
         initial="hidden"
         whileInView="visible"
         variants={slideUpVariants}
-        className='lg:w-[40%] w-full flex flex-col justify-center items-start lg:gap-6'
+        className="w-full  flex flex-col justify-center items-start gap-6 lg:w-[40%]"
       >
-        <p className='text-white text-lg text-justify'>At Demolahu Draughtsmanship & Construction Ltd, we bring expertise and innovation to the construction industry. Our company specializes in architectural and building drawings, construction works, land surveying, and cost estimation to help bring your projects to life with precision and efficiency.
-
-        Beyond construction, we provide comprehensive consultancy services for building permits, soil testing, environmental and fire safety certifications, planning schemes, land acquisition, and registration—ensuring your projects meet all regulatory standards.
+        <p className="text-white text-lg text-justify">
+          At Demolahu Draughtsmanship & Construction Ltd, we bring expertise and
+          innovation to the construction industry. Our company specializes in
+          architectural and building drawings, construction works, land
+          surveying, and cost estimation to help bring your projects to life
+          with precision and efficiency. Beyond construction, we provide
+          comprehensive consultancy services for building permits, soil testing,
+          environmental and fire safety certifications, planning schemes, land
+          acquisition, and registration—ensuring your projects meet all
+          regulatory standards.
         </p>
         <motion.button
           variants={zoomInVariants}
-          className='bg-turquoise hover:bg-white hover:text-black px-10 py-3 rounded-lg font-bold text-black'
-          onClick={() => navigate('/about')}
+          className="bg-turquoise hover:bg-white hover:text-black px-10 py-3 rounded-lg font-bold text-black"
+          onClick={() => navigate("/about")}
         >
           READ MORE
         </motion.button>
       </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
