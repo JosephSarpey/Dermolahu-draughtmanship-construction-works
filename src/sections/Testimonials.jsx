446f6efb112d565e80911957ae751bbc7ed603be
Emmanuel K. Mensah <ekmpizarro@gmail.com>
2025-03-24 18:58:06 +0000

    Modified accent color of testimonial component

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -14,11 +14,11 @@ const Testimonials = () => {
         variants={slideUpVariants}
         className='lg:w-[80%] w-[90%] m-auto py-[60px] flex flex-col justify-between items-center gap-[20px]'
       >
-        <motion.h1 variants={slideUpVariants} className='text-yellow-500 text-2xl'>TESTIMONIALS</motion.h1>
+        <motion.h1 variants={slideUpVariants} className='text-turquoise text-2xl'>TESTIMONIALS</motion.h1>
         <motion.h1 variants={slideUpVariants} className='text-white uppercase text-[40px] font-bold text-center'>WHAT OUR CLIENTS SAY</motion.h1>
         <motion.div
           variants={slideUpVariants}
-          className='w-[120px] h-[6px] bg-yellow-500'
+          className='w-[120px] h-[6px] bg-turquoise'
         ></motion.div>
 
         {/* make div for services mapping from export js file */}
@@ -31,14 +31,14 @@ const Testimonials = () => {
       {
         clients.map((item, index) => (
           <div key={index} className='flex flex-col justify-center items-center'>
-            <div className='border-2 border-white hover:bg-yellow-500 pb-[100px] pt-[30px]'>
+            <div className='border-2 border-white hover:bg-turquoise pb-[100px] pt-[30px]'>
               <p className='text-white hover:text-black text-lg text-center'>{item.about}</p>
             </div>
 
             <div className='flex flex-col justify-center items-center gap-[5px]'>
               <img className='mt-[-50px]' src={item.image} alt="client image" />
               <h1 className='text-white text-[27px] font-semibold uppercase'>{item.name}</h1>
-              <h1 className='text-yellow-500 text-[22px]'>{item.post}</h1>
+              <h1 className='text-turquoise text-[22px]'>{item.post}</h1>
             </div>
 
           </div>
